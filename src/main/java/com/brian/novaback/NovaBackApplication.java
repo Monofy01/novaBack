5bcc48a0b98dfc355f9125549247c2403aa138ea
monofy01 <bri_riva@hotmail.com>
2022-08-11 18:17:36 -0500

    [INIT] init project structure

---
+++ src/main/java/com/brian/novaback/NovaBackApplication.java
@@ -0,0 +1,13 @@
+package com.brian.novaback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NovaBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NovaBackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/brian/novaback/NovaBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brian.novaback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NovaBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
