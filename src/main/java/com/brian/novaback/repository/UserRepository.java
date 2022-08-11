b77349b82ec0d24cc009a28a2fd97c08eee4190c
monofy01 <bri_riva@hotmail.com>
2022-08-11 18:35:16 -0500

    [Added] repository class functionality [Added] service class functionality and mapped crud func by hibernate

--- src/main/java/com/brian/novaback/repository/UserRepository.java
+++ src/main/java/com/brian/novaback/repository/UserRepository.java
@@ -1,4 +1,13 @@
 package com.brian.novaback.repository;
 
-public class UserRepository {
+import com.brian.novaback.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    @Transactional
+    void deleteUserById(Long id);
 }
--- src/main/java/com/brian/novaback/service/UserService.java
+++ src/main/java/com/brian/novaback/service/UserService.java
@@ -1,4 +1,42 @@
 package com.brian.novaback.service;
 
+
+import com.brian.novaback.model.User;
+import com.brian.novaback.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class UserService {
+
+    private final UserRepository userRepository;
+
+    @Autowired
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    //CRUD FUNCTIONS MAPPED BY ORM
+    public User addUser(User user) {
+        return userRepository.save(user);
+    }
+
+    public List<User> findAllUsers() {
+        return userRepository.findAll();
+    }
+
+    public User updateUser(User user) {
+        return userRepository.save(user);
+    }
+
+    public void deleteByUserId(Long id) {
+        userRepository.deleteUserById(id);
+    }
+
+
+
+
+
 }
