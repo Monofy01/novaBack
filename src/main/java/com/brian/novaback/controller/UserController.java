02e0761e6d75e0262b91ed360844b9f393720076
monofy01 <bri_riva@hotmail.com>
2022-08-11 18:40:53 -0500

    [Added] controller class functionality [Added] app.properties config for local db

--- src/main/java/com/brian/novaback/controller/UserController.java
+++ src/main/java/com/brian/novaback/controller/UserController.java
@@ -1,4 +1,45 @@
 package com.brian.novaback.controller;
 
+
+import com.brian.novaback.model.User;
+import com.brian.novaback.service.UserService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@CrossOrigin
+@RequestMapping("/")
 public class UserController {
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping("/all")
+    public ResponseEntity<List<User>> getAllUsers() {
+        List<User> UserList = userService.findAllUsers();
+        return new ResponseEntity<>(UserList, HttpStatus.OK);
+    }
+
+    @PostMapping("/add")
+    public ResponseEntity<User> addUser(@RequestBody User User) {
+        User newUser = userService.addUser(User);
+        return new ResponseEntity<>(newUser, HttpStatus.CREATED);
+    }
+
+    @PutMapping("/update")
+    public ResponseEntity<User> updateUser(@RequestBody User User) {
+        User updateUser = userService.updateUser(User);
+        return new ResponseEntity<>(updateUser, HttpStatus.OK);
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<?> deleteUser(@PathVariable Long id) {
+        userService.deleteByUserId(id);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
 }
